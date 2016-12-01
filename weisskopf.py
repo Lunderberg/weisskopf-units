126c54fb0bf0b2b3d65b7266ea0da184bc25ddf3
Eric Lunderberg <lunderberg@nscl.msu.edu>
2016-12-01 09:57:49 -0500

    Removed dead code

--- weisskopf.py
+++ weisskopf.py
@@ -113,44 +113,6 @@ class MainWindow(QtGui.QMainWindow):
                                      '$')
 
 
-
-
-
-
-
-    # Here, defining a method.
-    # qt passes in additional arguments that this function does not need,
-    # so I use *args to ignore them.
-    def from_SaveButtonClicked(self,*args):
-        filetype = '.set'
-        filename = str(QtGui.QFileDialog.getSaveFileName(self,'Save Settings File','',
-                                                         'Settings file (*{})'.format(filetype)))
-
-        # filename is empty string if user hits cancel, so don't save anything
-        if filename:
-            if not filename.endswith(filetype):
-                filename += filetype
-            self.SaveSettings(filename)
-
-    def SaveSettings(self,filename):
-        print('Saving',filename)
-
-        # With statement automatically closes file at end.
-        with open(filename,'w') as f:
-            text_value = self.ui.text_entry.text()
-            f.write('Text field: {}\n'.format(text_value))
-
-            integer_value = self.ui.spin_integer.value()
-            f.write('Integer field: {}\n'.format(integer_value))
-
-            double_value = self.ui.spin_double.value()
-            f.write('Double field: {}\n'.format(double_value))
-
-            dropdown_index = self.ui.dropdown.currentIndex()
-            dropdown_text = self.ui.dropdown.currentText()
-            f.write('Dropdown: Index={}, Text={}\n'.format(dropdown_index,dropdown_text))
-
-
 if __name__=='__main__':
     # Initialize the qt event loop.
     app = QtGui.QApplication(sys.argv)
